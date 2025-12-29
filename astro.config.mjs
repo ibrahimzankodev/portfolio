a6aceb7aaeb8cbc6b41deadecd9f091479d60fb5
ibrahimzankodev <ibrahimzankodev@gmail.com>
2025-12-29 01:13:12 +0100

    feat: complete design personalization and layout overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: netlify()
 });
